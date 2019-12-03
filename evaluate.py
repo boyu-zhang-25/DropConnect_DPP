order parameter reproduced for SCM; gradient fixed for NN

evaluate.py
@@ -36,11 +36,6 @@ def get_Q(path_to_mask_list, path_to_teacher, input_dim):
 	teacher = pickle.load(open(path_to_teacher, 'rb'))
 	teahcer_w1 = teacher.w1.data.cpu().numpy() # teacher_hid_dim * input_dim
 	print('teacher w1 size:', teahcer_w1.shape)
-
-	'''
-	teahcer_w1 = np.load('/Users/mac/Desktop/pyscm/scm_erf_erf_N500_M2_K5_lr0.5_wd0_sigma0_bs1_i1steps800_s0_teacher.npy')
-	print('teacher loaded')
-	'''
 	
 	teacher_Q = np.dot(teahcer_w1, teahcer_w1.T) / input_dim
 	return expected_Q, unpurned_Q, teacher_Q
@@ -104,12 +99,7 @@ def get_R(path_to_student_mask, path_to_teacher, input_dim):
 	teahcer_w1 = teacher.w1.data.cpu().numpy().T # input_dim * teacher_hid_dim
 	teacher_hid_dim = teahcer_w1.shape[1]
 	print('teacher w1 size:', teahcer_w1.shape)
-
-	'''
-	teahcer_w1 = np.load('/Users/mac/Desktop/pyscm/scm_erf_erf_N500_M2_K5_lr0.5_wd0_sigma0_bs1_i1steps800_s0_teacher.npy')
-	print('teacher loaded')
-	teahcer_w1 = teahcer_w1.T
-	'''
+	
 
 	# get the expected R on purned student_w1
 	# student_hid_dim * teacher_hid_dim
@@ -153,7 +143,6 @@ def plot_R(expected_R, unpurned_R):
 	ax.set_title("unpurned_R")
 	fig.tight_layout()
 	plt.savefig('unpurned_R.png')
-
 
 
 def main():

teacher_dataset.py
@@ -30,7 +30,6 @@ class Teacher_dataset(Dataset):
 				sig_noise = 1):
 		super(Teacher_dataset, self).__init__()
 
-
 		inputs = torch.zeros((input_dim, num_data)) # input_dim * num_data
 		labels = torch.zeros(num_data)
 
@@ -82,7 +81,7 @@ def main():
 
 	# network parameter
 	parser.add_argument('--input_dim', type = int, help='The input dimension for each data point.')
-	parser.add_argument('--teacher_h_size', type = int, help='hidden layer size of the student MLP')
+	parser.add_argument('--teacher_h_size', type = int, help='hidden layer size of the student MLP.')
 	parser.add_argument('--num_data', type = int, help='Number of data points to be genrated.')
 	parser.add_argument('--mode', type = str, help='soft_committee or normal')
 	parser.add_argument('--sig_w', type = float, help='scaling variable for the output noise.')

teacher_student.py
@@ -76,27 +76,13 @@ class student_MLP(nn.Module):
 	def forward(self, x):
 
 		h = self.w1(x)
-
 		h_norm = h / math.sqrt(self.input_dim)
 		h_norm_new = h_norm / math.sqrt(2)
 		a = torch.erf(h_norm_new)
-
 		output = self.w2(a)
-
-		'''
-		h = self.activation(self.w1(x))
-		output = self.w2(h)
-		'''
 
 		return output
 
-	# 
-	def g(self, x):
-		h = self.w1(x)
-		h_norm = h / math.sqrt(self.input_dim)
-		h_norm_new = h_norm / math.sqrt(2)
-		a = torch.erf(h_norm_new)
-		return a		
 
 
 # training loop
@@ -113,15 +99,9 @@ def train(args, model, device, train_loader, criterion, optimizer, epoch):
 		data, target = train_loader[idx]
 		data, target = data.to(device), target.to(device)
 
-		# print('w1 old:', model.w1.weight.data)
-		# print('w2 old:', model.w2.weight.data)
-
-		# print('data:', data)
-		# print('target:', target)
-
 		optimizer.zero_grad()
 		output = model(data)
-		# print('output:', output.item())
+
 
 		# print(output.shape, target.shape)
 		loss = criterion(output, target.view(-1))
@@ -135,9 +115,13 @@ def train(args, model, device, train_loader, criterion, optimizer, epoch):
 
 		# manually scale gradient for auto grad
 		# following the paper [S. Goldt, 2019]
-		model.w1.weight.grad = model.w1.weight.grad * math.sqrt(args.input_dim) / 2
 		if model.w2.weight.requires_grad:
-			model.w2.weight.grad = model.w2.weight.grad / 2
+
+			# model.w2.weight.grad = model.w2.weight.grad / 2
+			model.w1.weight.grad = model.w1.weight.grad * math.sqrt(args.input_dim) / 2
+
+		else:
+			model.w1.weight.grad = model.w1.weight.grad * math.sqrt(args.input_dim) / 2
 		
 		# print('w2 grad:', model.w2.weight.grad.numpy())
 		# print('************')
@@ -315,7 +299,6 @@ def main():
 
 			file_name = 'student_masks_' + args.pruning_choice + '_' + str(args.student_h_size) + '.pkl'
 			pickle.dump((unpurned_MLP, mask_list), open(file_name, "wb"))
-
 
 
 if __name__ == '__main__':