b846875d8192088f098acd8bfbf7c492659f90df
Boyu Zhang <bzhang25@u.rochester.edu>
2020-02-04 12:27:18 -0500

    random edge proof modified; figures done

--- plot_loss.py
+++ plot_loss.py
@@ -20,6 +20,7 @@ def calculate_param(student_hidden_size, inp_dim):
 
 xticks = ['1:83', '2:166', '3:250', '4:333', '5:417']
 
+# edge
 all_edge_noiseless = np.asarray(all_edge_noiseless)
 noiseless_edge_std = np.std(all_edge_noiseless, axis=0)
 noiseless_edge_means = np.mean(all_edge_noiseless, axis=0)	
@@ -28,6 +29,7 @@ all_edge_noise0_25 = np.asarray(all_edge_noise0_25)
 noise0_25_edge_std = np.std(all_edge_noise0_25, axis=0)
 noise0_25_edge_means = np.mean(all_edge_noise0_25, axis=0)
 
+# node
 all_node_noiseless = np.asarray(all_node_noiseless)
 noiseless_node_std = np.std(all_node_noiseless, axis=0)
 noiseless_node_means = np.mean(all_node_noiseless, axis=0)	
@@ -36,18 +38,33 @@ all_node_noise0_25 = np.asarray(all_node_noise0_25)
 noise0_25_node_std = np.std(all_node_noise0_25, axis=0)
 noise0_25_node_means = np.mean(all_node_noise0_25, axis=0)
 
+# rand edge
+all_rand_noiseless = np.asarray(all_rand_noiseless)
+noiseless_rand_std = np.std(all_rand_noiseless, axis=0)
+noiseless_rand_means = np.mean(all_rand_noiseless, axis=0)	
+
+all_rand_noise0_25 = np.asarray(all_rand_noise0_25)
+noise0_25_rand_std = np.std(all_rand_noise0_25, axis=0)
+noise0_25_rand_means = np.mean(all_rand_noise0_25, axis=0)
+
 x = [k for k in range(5)]
-rand_edge = [3.3552032455837146, 2.0042477849312137, 1.073201504878175, 0.46989968915832225, 0.116993396833319]
 
 plt.figure(1)
 plt.errorbar(x, noiseless_edge_means, yerr=noiseless_edge_std, marker = '^', label="Avg. DPP_Edge Loss")
 plt.errorbar(x, noiseless_node_means, yerr=noiseless_node_std, marker = 's', label="Avg. DPP_Node Loss")
-# plt.plot(x, rand_edge, marker = 'o', label="Random_Edge")
+plt.errorbar(x, noiseless_rand_means, yerr=noiseless_rand_std, marker = 'o', label="Avg. Rand_Edge Loss")
+
+print("noiseless edge data")
 print(noiseless_edge_means)
 print(noiseless_edge_std)
 
+print("noiseless node data")
 print(noiseless_node_means)
 print(noiseless_node_std)
+
+print("noiseless rand edge data")
+print(noiseless_rand_means)
+print(noiseless_rand_std)
 
 plt.plot(x, [noiseless_unpruned_loss for _ in range(5)], dashes=[6, 2], label="Unpruned Loss")
 plt.legend(loc = "best")
@@ -62,11 +79,19 @@ plt.savefig('Noiseless Average Test Loss', dpi = 200)
 plt.figure(2)
 plt.errorbar(x, noise0_25_edge_means, yerr=noise0_25_edge_std, marker = '^', label="Avg. DPP_Edge Loss")
 plt.errorbar(x, noise0_25_node_means, yerr=noise0_25_node_std, marker = 's', label="Avg. DPP_Node Loss")
+plt.errorbar(x, noise0_25_rand_means, yerr=noise0_25_rand_std, marker = 'o', label="Avg. Rand_Edge Loss")
+
+print("noiseless edge data")
 print(noise0_25_edge_means)
 print(noise0_25_edge_std)
 
+print("noiseless node data")
 print(noise0_25_node_means)
 print(noise0_25_node_std)
+
+print("noiseless rand edge data")
+print(noise0_25_rand_means)
+print(noise0_25_rand_std)
 
 plt.plot(x, [noise0_25_unpruned_loss for _ in range(5)], dashes=[6, 2], label="Unpruned Loss")
 plt.legend(loc = "best")
