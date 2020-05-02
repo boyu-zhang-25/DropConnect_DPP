cd33b1dccf29178e7ada0a0a058c359068887add
Boyu Zhang <bzhang25@u.rochester.edu>
2020-05-02 17:48:56 -0400

    clean up data

--- CIFAR_dpp/CIFAR.py
+++ CIFAR_dpp/CIFAR.py
@@ -252,12 +252,12 @@ def main():
 		# the CIFAR dataset
 		transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-		trainset = datasets.CIFAR10(root='.../data', train=True,
+		trainset = datasets.CIFAR10(root='./data', train=True,
 												download=True, transform=transform)
 		train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.train_batch_size,
 												  shuffle=False, **kwargs)
 
-		testset = datasets.CIFAR10(root='.../data', train=False,
+		testset = datasets.CIFAR10(root='./data', train=False,
 											   download=True, transform=transform)
 		test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
 												 shuffle=False, **kwargs)
