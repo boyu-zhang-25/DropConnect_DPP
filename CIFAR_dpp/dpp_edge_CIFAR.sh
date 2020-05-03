bcd22811dd7aa2f361a20d196d0d4974f899f947
Boyu Zhang <bzhang25@u.rochester.edu>
2020-05-03 16:00:38 -0400

    getting error bars for all settings in DIVNET ICLR 2016

--- CIFAR_dpp/dpp_edge_CIFAR.sh
+++ CIFAR_dpp/dpp_edge_CIFAR.sh
@@ -1,13 +1,11 @@
-vars='83 166 250 333 417'
+vars='308 615 922 1229 1536 1844 2151 2458 2765'
 rounds='1'
-teacher_path='teacher_noise0.25.pkl'
 for round in $rounds
 do
-	filename="dpp_edge_output_lr0.5_v4_800000_noise0.25_round${round}.txt"
+	filename="dpp_edge_CIFAR_0.0_batch128_output_round${round}.txt"
 	for var in $vars
 	do
 		echo '============================================================' 2>> $filename
-		python3 teacher_student.py --input_dim 500 --student_h_size 6 --teacher_path $teacher_path  --nonlinearity sigmoid --pruning_choice dpp_edge  --mode normal  --trained_weights student_6.pth --procedure pruning --num_masks 100 --k $var >> $filename
-		python3 teacher_student.py --input_dim 500 --student_h_size 6 --teacher_path $teacher_path  --nonlinearity sigmoid  --mode normal  --trained_weights student_6.pth --procedure testing --pruning_choice dpp_edge --k $var >> $filename
+		python3 CIFAR.py --pruning_choice dpp_edge --k $var --procedure pruning >> $filename
 	done
 done
