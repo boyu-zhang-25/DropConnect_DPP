fae6f8b18bb9acdbc0601e374eb1b0d1dcf52a47
racharyy <rupamacharyya@gmail.com>
2020-05-27 11:03:12 -0400

    calc param updated

--- mnist_dpp/cal_param_MNIST.py
+++ mnist_dpp/cal_param_MNIST.py
@@ -3,9 +3,15 @@ import math
 def cal_param_MNIST(input_dim, hid_dim):
 
 	percent = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-	node = [int(p * hid_dim) for p in percent]
 
-	edge = [math.ceil(n * input_dim / hid_dim) for n in node]
+	edge = [int(p * input_dim) for p in percent]
+
+	node = [math.ceil(hid_dim * (e + hid_dim)/(input_dim + hid_dim)) for e in edge]
+
+
+	# node = [int(p * hid_dim) for p in percent]
+
+	# edge = [math.ceil(n * input_dim / hid_dim) for n in node]
 	print(node)
 	print(edge)
 
